474f996e03bb3fb60dd9fc37303033500281f04e
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-02-23 13:45:52 +0000

    MVA ccor

--- 01_draft/00_core_functions.py
+++ 01_draft/00_core_functions.py
@@ -1,44 +1,129 @@
-import numpy as np
-import pandas as pd
-from numpy.linalg import inv
+def GWAS_linear_combination_Z_based(a, Z, covm, eaf, N):
+    """
+    This function performs a linear combination of genome-wide association studies (GWAS) based on their Z-scores,
+    phenotypic correlations, and effective allele frequencies (EAF).
 
-def GWAS_linear_combination_v2(a, beta_a, se, var_y=None, covm=None, N=None):
-    snp_row = np.arange(beta_a.shape[0])
-    if var_y is None:
-        var_y = np.ones(a.shape[0])
-    
-    for i in range(var_y.shape[0]):
-        beta_a[:, i] = beta_a[:, i] / np.sqrt(var_y[i])
-        se[:, i] = se[:, i] / np.sqrt(var_y[i])
+    Parameters:
+    -----------
+    a : numpy array, shape (1, m)
+        The array of linear coefficients for the GWAS, where `m` is the number of GWAS.
 
-    if covm is not None:
-        covm = (1 / (np.sqrt(np.diag(covm))[:, np.newaxis] @ np.sqrt(np.diag(covm))[np.newaxis, :])) * covm
+    Z : numpy array, shape (M, m)
+        The array of Z-scores for the GWAS, where `M` is the number of SNPs and `m` is the number of GWAS.
 
-    var_y_a = np.sum(covm * (a[:, np.newaxis] @ a[np.newaxis, :]))
-    b = beta_a @ a
-    varb = np.array([np.sum(covm * (se[i, np.newaxis] @ se[i, np.newaxis].T) * (a[:, np.newaxis] @ a[np.newaxis, :])) for i in snp_row])
-    sen = np.sqrt(varb)
-    b = b / np.sqrt(var_y_a)
-    sen = sen / np.sqrt(var_y_a)
-    
-    ind = np.where(np.abs(b / sen) < 2)[0]
-    N_from_se = np.median(1 / (sen[ind]**2))
-    
-    out = pd.DataFrame({'b': b, 'se': sen, 'N': N_from_se})
-    
-    return out
+    covm : numpy array, shape (m, m)
+        The phenotypic correlation matrix between the GWAS.
 
+    eaf : numpy array, shape (1, M)
+        The array of effective allele frequencies for each SNP, where `M` is the number of SNPs.
 
-def GWAS_linear_combination_Z_based(a, Z, covm, N, eaf):
-    snp_row = np.arange(Z.shape[0])
-    var_y = np.ones(a.shape[0])
-    se_a = np.apply_along_axis(lambda x: np.sqrt(1 / (x[0]**2 + x[1])), axis=1, arr=np.column_stack((Z, N)))
-    beta_a = np.apply_along_axis(lambda x: x[0] * se_a[x[2], :], axis=1, arr=np.column_stack((Z, N, snp_row)))
-    
-    out = GWAS_linear_combination_v2(a=a, beta_a=beta_a, se=se_a, var_y=None, covm=covm, N=N)
-    
+    N : numpy array, shape (M, m)
+        The array of total sample sizes for each SNP in each GWAS, where `M` is the number of SNPs and `m` is the
+        number of GWAS.
+
+    Returns:
+    --------
+    out : pandas DataFrame
+        The DataFrame containing the results of the linear combination of the GWAS. The columns are:
+
+        - `beta` : numpy array, shape (M,)
+            The array of effect sizes for each SNP.
+
+        - `se` : numpy array, shape (M,)
+            The array of standard errors for each effect size.
+
+        - `pval` : numpy array, shape (M,)
+            The array of p-values for each effect size.
+
+        - `n_total` : float
+            The effective sample size of the linear combination of the GWAS.
+    """
+    from scipy.stats import chi2
+
+    # Calculate standard errors from Z-scores
+    SE = np.sqrt(1 / (Z ** 2 + N))
+    # Calculate betas from Z-scores
+    BETA = Z * SE
+    # Calculate variance of y
+    vary = np.sum(covm * np.outer(a, a))
+    # Calculate linear combination of betas
+    b = np.matmul(BETA, a)
+    # Calculate variance of b
+    def varb_lin(i):
+        sei=SE[i,:]
+        out=np.sum(covm*np.outer(sei,sei)*np.outer(a,a))
+        return out
+    varb=[varb_lin(i) for i in range(0,Z.shape[0])]
+    varb=np.array(varb)
+    seb=np.sqrt(varb)
+    # Calculate effective sample size
+    ind = np.where(np.abs(b / seb) < 2)
+    N_eff = np.median(1 / (seb[ind] ** 2))
+    # Calculate standard error and beta for variance normalization
     varg = 2 * (1 - eaf) * eaf
-    out['b'] = out['b'] / np.sqrt(varg)
-    out['se'] = out['se'] / np.sqrt(varg)
-    
+    b_norm = b / np.sqrt(varg)
+    seb_norm = seb / np.sqrt(varg)
+    # Calculate p-values using chi-squared distribution
+    pval = chi2.sf((b_norm / seb_norm) ** 2, 1)
+    # Create output DataFrame
+    out = pd.DataFrame({'beta': b_norm, 'se': seb_norm, 'pval': pval, 'n_total': N_eff})
     return out
+
+
+def GWAS_linear_combination_Z_based_OLD(a, Z, covm, eaf, N):
+    """
+    This fucntion performs linear combination of GWAS based on their Z-scores,phenotypic corrs, and eaf
+    m is the number of GWAS, M is the number of SNPs
+    a is the np array of linera coefficients 1xm
+    Z is np array of Z scores Mxm
+    Z is np array of N total Mxm
+    covm is phenotypic coreralashion matrix mxm
+    eaf is the np array of EAF for each SNP 1xM
+    """
+    #
+    m=Z.shape[1]
+    #
+    def se_from_z(i):
+        z=Z[:,i]
+        n=N[:,i]
+        se_x=np.sqrt(1/(z**2+n))
+        return se_x
+    SE=[se_from_z(i) for i in range(0,m)]
+    SE=np.array(SE)
+    SE=np.transpose(SE)
+    #   
+    def beta_from_z(i):
+        z=Z[:,i]
+        se=SE[:,i]
+        beta=z*se
+        return beta
+    BETA=[beta_from_z(i) for i in range(0,m)]
+    BETA=np.array(BETA)
+    BETA=np.transpose(BETA)
+    #
+    vary=np.sum(covm*np.outer(a,a))
+    b=np.matmul(BETA,a)
+    #
+    def varb_lin(i):
+        sei=SE[i,:]
+        out=np.sum(covm*np.outer(sei,sei)*np.outer(a,a))
+        return out
+    varb=[varb_lin(i) for i in range(0,Z.shape[0])]
+    varb=np.array(varb)
+    seb=np.sqrt(varb)
+    #
+    b=b/np.sqrt(vary)
+    seb=seb/np.sqrt(vary)
+    #
+    ind = np.where(np.abs(b / seb) < 2)
+    N_eff = np.median(1 / (seb[ind] ** 2))
+    #
+    varg = 2 * (1 - eaf) * eaf
+    b=b/np.sqrt(varg)
+    seb=seb/np.sqrt(varg)
+    #
+    from scipy.stats.distributions import chi2
+    pval=chi2.sf((b/seb)**2,1)
+    out=pd.DataFrame({'beta': b, 'se': seb, 'pval':pval, 'n_total':N_eff})
+    return out
+
---
+++ 01_draft/04_mva.py
@@ -0,0 +1,92 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+import pyspark.sql.types as t
+from pyspark.sql import Window
+import scipy as sc
+from scipy import stats
+import numpy as np
+import pandas as pd
+
+
+#Spark initialization and configuration
+
+global spark
+spark = (
+    SparkSession.builder
+    .master('local[*]')
+    .config('spark.driver.memory', '15g')
+    .appName('spark')
+    .getOrCreate()
+)
+
+#variant_annotation = spark.read.parquet("gs://genetics-portal-dev-data/22.09.0/outputs/lut/variant-index")
+#variant_annotation = (variant_annotation
+#               .withColumn("id", f.concat_ws("_", f.col("chr_id"), f.col("position"), f.col("ref_allele"), f.col("alt_allele")))
+#               )
+
+h2=pd.read_csv("h2.csv")
+phe=pd.read_csv("phen_corr.csv",header=None)
+
+SIDS=h2["study_id"].values
+
+gw=SIDS[0]
+print("N "+str(0)+": "+gw)
+gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+gwas=spark.read.parquet(gw)
+gwas=(gwas
+    .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+    .withColumn("zscore", f.col("beta")/f.col("se"))
+    )
+gwas=(gwas
+    .filter(f.col("eaf")>=0.001)
+    .filter(f.col("eaf")<=0.999)
+    )
+DF=gwas.select("id","zscore","eaf","n_total",'chrom','pos','ref','alt')
+DF=(DF
+    .withColumnRenamed("zscore", "z1")
+    .withColumnRenamed("n_total", "n1")
+    )
+
+i=2
+for gw in SIDS[1:]:
+    print("N: "+gw)
+    gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+    gwas=spark.read.parquet(gw)
+    gwas=(gwas
+        .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+        .withColumn("zscore", f.col("beta")/f.col("se"))
+        )
+    gwas=(gwas
+        .filter(f.col("eaf")>=0.001)
+        .filter(f.col("eaf")<=0.999)
+        )
+    gwas=gwas.select("id","zscore","n_total")
+    gwas=(gwas
+        .withColumnRenamed("zscore", ("z"+str(i)))
+        .withColumnRenamed("n_total", ("n"+str(i)))
+    )
+    i=i+1
+    DF = DF.join(gwas, on = "id", how = "inner")
+
+DF=DF.toPandas()
+Z=DF[["z1","z2","z3","z4"]]
+Z=np.array(Z)
+
+N=DF[["n1","n2","n3","n4"]]
+N=np.array(N)
+
+a=[0.4980116,0.3785392,0.2401656,0.2578737]
+a=np.array(a)
+
+phe=np.array(phe)
+
+eaf=DF["eaf"]
+eaf=np.array(eaf)
+
+
+MVA=GWAS_linear_combination_Z_based(a=a,Z=Z,covm=phe,eaf=eaf,N=N)
+MVA2=GWAS_linear_combination_Z_based_v2(a=a,Z=Z,covm=phe,eaf=eaf,N=N)
+
+df=DF[["id","chrom","pos",""]]
+
+
