10124d5b42f46576abed8efcab7323e97d025145
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-04-03 16:06:52 +0100

    adding files 4

---
+++ 03_mva_draft/01_draft_run.py
@@ -0,0 +1,143 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+import pyspark.sql.types as t
+from pyspark.sql import Window
+import scipy as sc
+from scipy import stats
+import numpy as np
+import pandas as pd
+import re
+
+#Spark initialization and configuration
+
+global spark
+spark = (
+    SparkSession.builder
+    .master('local[*]')
+    .config('spark.driver.memory', '15g')
+    .appName('spark')
+    .getOrCreate()
+)
+
+variant_annotation = spark.read.parquet("gs://genetics-portal-dev-data/22.09.1/outputs/lut/variant-index")
+variant_annotation = (variant_annotation
+               .withColumn("id", f.concat_ws("_", f.col("chr_id"), f.col("position"), f.col("ref_allele"), f.col("alt_allele")))
+               )
+variant_annotation=variant_annotation.select("rs_id","id")
+
+h2=pd.read_csv("~/projects/Sanger_OT_MVA/03_mva_draft/all_h2.csv",sep="\t")
+h2["Trait"]=[re.sub(string=x,pattern=".results",repl="") for x in h2["Trait"]]
+list_of_traits_in_h2=list(h2["Trait"])
+
+GCOR=pd.read_csv("~/projects/MVA_output/01_clusters_draft/df_1222.csv",index_col=0)
+list_of_traits_in_gcor=list(GCOR.columns)
+
+#self checking
+sum([elem in list_of_traits_in_h2 for elem in list_of_traits_in_gcor])
+
+
+clusters=pd.read_csv("~/projects/Sanger_OT_MVA/03_mva_draft/Study_and_cluster.csv")
+list_of_traits_in_clusters=list(clusters.iloc[:,0])
+sum([elem in list_of_traits_in_gcor for elem in list_of_traits_in_clusters])
+clusters=clusters[clusters.iloc[:,0].isin(list_of_traits_in_gcor)]
+
+
+number_of_clusters=clusters["x"].max()
+
+
+for i in range(1,number_of_clusters):
+    y=clusters[clusters.iloc[:,1]==i]
+    if len(y)>1:
+        list_of_ids=list(y.iloc[:,0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+gw=SIDS[0]
+print("N "+str(0)+": "+gw)
+gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+gwas=spark.read.parquet(gw)
+gwas=(gwas
+    .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+    .withColumn("zscore", f.col("beta")/f.col("se"))
+    )
+gwas=(gwas
+    .filter(f.col("eaf")>=0.001)
+    .filter(f.col("eaf")<=0.999)
+    )
+DF=gwas.select("id","zscore","eaf","n_total",'chrom','pos','ref','alt')
+DF=(DF
+    .withColumnRenamed("zscore", "z1")
+    .withColumnRenamed("n_total", "n1")
+    )
+
+i=2
+for gw in SIDS[1:]:
+    print("N: "+gw)
+    gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+    gwas=spark.read.parquet(gw)
+    gwas=(gwas
+        .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+        .withColumn("zscore", f.col("beta")/f.col("se"))
+        )
+    gwas=(gwas
+        .filter(f.col("eaf")>=0.001)
+        .filter(f.col("eaf")<=0.999)
+        )
+    gwas=gwas.select("id","zscore","n_total")
+    gwas=(gwas
+        .withColumnRenamed("zscore", ("z"+str(i)))
+        .withColumnRenamed("n_total", ("n"+str(i)))
+    )
+    i=i+1
+    DF = DF.join(gwas, on = "id", how = "inner")
+
+
+l=DF.join(variant_annotation, on="id", how="inner").distinct()
+DF=l
+DF=DF.toPandas()
+
+sum(DF["z1"]**2>=29.71679)
+sum(DF["z2"]**2>=29.71679)
+sum(DF["z3"]**2>=29.71679)
+sum(DF["z4"]**2>=29.71679)
+
+Z=DF[["z1","z2","z3","z4"]]
+Z=np.array(Z)
+
+N=DF[["n1","n2","n3","n4"]]
+N=np.array(N)
+
+h2=np.array(h2["h2"])
+gcor=np.array(gcor)
+#a=[0.4980116,0.3785392,0.2401656,0.2578737]
+#a=np.array(a)
+
+phe=np.array(phe)
+
+eaf=DF["eaf"]
+eaf=np.array(eaf)
+
+from core_functions import GWAS_linear_combination_Z_based
+from core_functions import GIP1_lin_comb_Z_based
+#from core_functions import GWAS_linear_combination_Z_based_OLD
+
+#MVA=GWAS_linear_combination_Z_based(a=a,Z=Z,covm=phe,eaf=eaf,N=N)
+#MVA2=GWAS_linear_combination_Z_based_OLD(a=a,Z=Z,covm=phe,eaf=eaf,N=N)
+MVA=GIP1_lin_comb_Z_based(Z=Z,covm=phe,eaf=eaf,N=N, gcor=gcor, h2=h2)
+
+
+df=DF[["id","chrom","pos","ref","alt","rs_id"]]
+df=pd.concat([df,MVA], axis=1)
+
+df.to_csv("/home/yt4/projects/SS_QC/CVD_MVA_GIP1_with_rsid.csv",index=False)
+
--- 03_mva_draft/core_functions.py
+++ 03_mva_draft/core_functions.py
@@ -2,6 +2,95 @@ import numpy as np
 from scipy.stats import chi2
 import pandas as pd
 
+def phe_corr(list_of_ids):
+    from pyspark.sql import SparkSession
+    import pyspark.sql.functions as f
+    import pyspark.sql.types as t
+    from pyspark.sql import Window
+    import scipy as sc
+    from scipy import stats
+    import numpy as np
+    import pandas as pd
+    from scipy.stats import chi2
+
+    #
+    global spark
+    spark = (
+        SparkSession.builder
+        .master('local[*]')
+        .config('spark.driver.memory', '15g')
+        .appName('spark')
+        .getOrCreate()
+    )
+
+    gw=list_of_ids[0]
+    print("N "+str(0)+": "+gw)
+    gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+    gwas=spark.read.parquet(gw)
+    gwas=(gwas
+        .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+        .withColumn("zscore", f.col("beta")/f.col("se"))
+        )
+    gwas=(gwas
+        .filter((f.col("zscore")**2)<=4)
+        .filter(f.col("eaf")>=0.01)
+        .filter(f.col("eaf")<=0.99)
+        )
+    Z=gwas.select("id","zscore")
+
+    for gw in list_of_ids[1:]:
+        print("N: "+gw)
+        gw='gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+        gwas=spark.read.parquet(gw)
+        gwas=(gwas
+            .withColumn("id", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+            .withColumn("zscore", f.col("beta")/f.col("se"))
+            )
+        gwas=(gwas
+            .filter((f.col("zscore")**2)<=4)
+            .filter(f.col("eaf")>=0.01)
+            .filter(f.col("eaf")<=0.99)
+            )
+        gwas=gwas.select("id","zscore")
+        Z = Z.join(gwas, on = "id", how = "inner")
+
+
+    from pyspark.sql.functions import monotonically_increasing_id
+
+    # add unique identifier to column names
+    df = Z.toDF(*[f"{col}_{i}" for i, col in enumerate(Z.columns)])
+    # rename columns
+    df = df \
+        .withColumnRenamed("id_0", "id") \
+        .withColumnRenamed("zscore_1", "z1") \
+        .withColumnRenamed("zscore_2", "z2") \
+        .withColumnRenamed("zscore_3", "z3") \
+        .withColumnRenamed("zscore_4", "z4")
+
+    # drop unique identifier column
+    df = df.drop("id_1")
+    Z=df.select('z1', 'z2', 'z3', 'z4')
+
+    from pyspark.ml.stat import Correlation
+    from pyspark.ml.feature import VectorAssembler
+
+    # Create a VectorAssembler to combine the columns into a single vector column
+    assembler = VectorAssembler(inputCols=['z1', 'z2', 'z3', 'z4'], outputCol='features')
+
+    # Apply the VectorAssembler to the DataFrame
+    data = assembler.transform(Z).select('features')
+
+    # Calculate the correlation matrix using the Pearson method
+    corr_matrix = Correlation.corr(data, 'features', method='pearson').head()
+    corr_matrix_array = corr_matrix[0].toArray()
+
+    # Print the correlation matrix
+    #print(corr_matrix_array)
+    #np.linalg.det(corr_matrix_array)
+
+    #np.savetxt('phen_corr.csv', corr_matrix_array, delimiter=',')
+    return corr_matrix_array
+
 
 def GIP1_lin_comb_Z_based(Z, covm, eaf, N, gcor, h2):
     import numpy as np
