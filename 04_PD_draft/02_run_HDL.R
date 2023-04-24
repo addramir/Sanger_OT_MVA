cf8c9306b34a40fc156ca5d5741fbcb19c1150f1
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-04-24 16:45:00 +0100

    af31

--- 04_PD_draft/02_run_HDL.R
+++ 04_PD_draft/02_run_HDL.R
@@ -15,7 +15,7 @@ for (i in 1:(n-1)){
 	for (j in (i+1):n){
 		gwas1=fread(lst[i],data.table=F)
 		gwas2=fread(lst[j],data.table=F)
-		res.HDL <- HDL.rg.parallel(gwas1, gwas2, LD.path, numCores = 6)
+		res.HDL <- HDL.rg.parallel(gwas1, gwas2, LD.path, numCores = 8)
 
 		out_rg[i,j]=out_rg[j,i]=res.HDL$rg
 		out_pval[i,j]=out_pval[j,i]=res.HDL$P
@@ -29,31 +29,6 @@ for (i in 1:(n-1)){
 save(list=c("out_rg","out_pval","h2","h2_se"),file="~/projects/MVA_output/02_PD_gwas/v1_hm3_matrix.RData")
 
 ######
-
-LD.path <- "~/projects/MVA_output/02_PD_gwas/UKB_imputed_hapmap2_SVD_eigen99_extraction"
-
-out_rg=array(NA,c(n,n))
-out_pval=array(NA,c(n,n))
-h2=rep(NA,n)
-h2_se=rep(NA,n)
-for (i in 1:(n-1)){
-	for (j in (i+1):n){
-		gwas1=fread(lst[i],data.table=F)
-		gwas2=fread(lst[j],data.table=F)
-		res.HDL <- HDL.rg.parallel(gwas1, gwas2, LD.path, numCores = 6)
-
-		out_rg[i,j]=out_rg[j,i]=res.HDL$rg
-		out_pval[i,j]=out_pval[j,i]=res.HDL$P
-		h2[i]=res.HDL$estimates.df[1,"Estimate"]
-		h2[j]=res.HDL$estimates.df[2,"Estimate"]
-		h2_se[i]=res.HDL$estimates.df[1,"se"]
-		h2_se[j]=res.HDL$estimates.df[2,"se"]
-	}
-}
-
-save(list=c("out_rg","out_pval","h2","h2_se"),file="~/projects/MVA_output/02_PD_gwas/v1_hm3_matrix.RData")
-
-
 
 LD.path <- "~/projects/MVA_output/02_PD_gwas/UKB_imputed_hapmap2_SVD_eigen99_extraction"
 
