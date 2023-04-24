45d9022e72400362ef6db4ae2c5d2f80f40c9763
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-04-24 11:51:59 +0100

    af29

--- 04_PD_draft/core_functions.py
+++ 04_PD_draft/core_functions.py
@@ -44,3 +44,29 @@ def save_gwas_hm3_snps_HDL(list_of_ids, max_ram_to_use, path_out):
     finally:
         spark.stop()
         print("Done!")
+
+def save_gwas_all_snps_HDL(list_of_ids, max_ram_to_use, path_out):
+    try:
+        spark = get_spark_session(max_ram_to_use)
+        variant_index = spark.read.parquet("gs://genetics-portal-dev-data/22.09.1/outputs/lut/variant-index")
+        variant_index = (
+            variant_index
+            .withColumn("snpid", f.concat_ws("_", f.col("chr_id"), f.col("position"), f.col("ref_allele"), f.col("alt_allele")))
+        )
+        VI = variant_index.select(f.col("chr_id_b37"), f.col("position_b37"), f.col("rs_id"), f.col("snpid"))
+        cts = ['rs_id', 'ref', 'alt', 'beta', 'se', 'n_total']
+        for i, gw in enumerate(list_of_ids):
+            print("N "+str(i)+": "+gw)
+            gwl = 'gs://genetics-portal-dev-sumstats/unfiltered/gwas/'+gw+'.parquet/'
+            gwas = spark.read.parquet(gwl)
+            gwas = gwas.withColumn("snpid", f.concat_ws("_", f.col("chrom"), f.col("pos"), f.col("ref"), f.col("alt")))
+            l = gwas.join(VI, on="snpid", how="inner").distinct()
+            l = l.select(cts)
+            L = l.toPandas()
+            L.columns = ['SNP', 'A2', 'A1', 'b', 'se', 'N']
+            L.to_csv(path_out+gw+".txt", sep="\t", index=False)
+    except Exception as e:
+        print("Error occurred: {}".format(str(e)))
+    finally:
+        spark.stop()
+        print("Done!")
