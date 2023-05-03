f9fc9b3e8c2d23063c9dca7d99b4ca09978cf1f6
Yakov Tsepilov <yt4@sanger.ac.uk>
2023-05-03 17:35:16 +0100

    af04

---
+++ 04_PD_draft/10_clumping.R
@@ -0,0 +1,103 @@
+library("data.table")
+setwd("~/projects/MVA_output/02_PD_gwas/full_gwas/")
+source("~/projects/Sanger_OT_MVA/04_PD_draft/shared_heredity.R")
+
+#full
+fl=fread("../PD_gwas/nallsEtAl2019_excluding23andMe_allVariants.tab",data.table=F)
+
+l=strsplit(fl$SNP,split=":")
+
+l=unlist(l)
+chr=l[seq(1,length(l),by=2)]
+chr=gsub(chr,pattern="chr",repl="")
+chr=as.numeric(chr)
+
+pos=l[seq(2,length(l),by=2)]
+pos=as.numeric(pos)
+
+maf=pmin(fl$freq,1-fl$freq)
+fl=cbind(fl,chr,pos,maf)
+
+fl_maf=fl[maf>=0.001,]
+
+n_f=function_for_shlop_29_03_2020(locus_table=fl,p_value="p",pos="pos",snp="SNP", delta=5e5,chr="chr",thr=5e-8,trait=NULL)
+dim(n_f)
+#[1] 32 12
+n_f_full=cbind(n_f,trait="full")
+
+
+####
+#with rsid
+fl=fread("nallsEtAl2019_excluding23andMe.txt",data.table=F)
+ref=fread("NEALE2_20107_11.txt",data.table=F)
+ind=match(fl$SNP,ref$SNP)
+table(fl$SNP==ref$SNP[ind])
+
+fl=cbind(fl,pos=ref$pos[ind],chr=ref$chr[ind])
+maf=pmin(fl$eaf,1-fl$eaf)
+fl=fl[maf>=0.001,]
+dim(fl)
+#[1] 10782058       10
+n_f=function_for_shlop_29_03_2020(locus_table=fl,p_value="pval",pos="pos",snp="SNP", delta=5e5,chr="chr",thr=5e-8,trait=NULL)
+dim(n_f)
+#[1] 27 10
+n_f_full_rsid=cbind(n_f,trait="full_rsid")
+
+####
+#with rsid
+fl=fread("GIP1_5_traits.txt",data.table=F)
+head(fl)
+maf=pmin(fl$eaf,1-fl$eaf)
+fl=fl[maf>=0.001,]
+dim(fl)
+#[1] 8650201      10
+n_f=function_for_shlop_29_03_2020(locus_table=fl,p_value="pval",pos="pos",snp="SNP", delta=5e5,chr="chr",thr=5e-8,trait=NULL)
+dim(n_f)
+#[1] 23 10
+
+####
+#with rsid
+fl=fread("GIP1_4_traits.txt",data.table=F)
+head(fl)
+maf=pmin(fl$eaf,1-fl$eaf)
+fl=fl[maf>=0.001,]
+dim(fl)
+#[1] 7992854       8
+
+ind=match(fl$SNP,ref$SNP)
+table(fl$SNP==ref$SNP[ind])
+fl=cbind(fl,pos=ref$pos[ind],chr=ref$chr[ind])
+
+n_f=function_for_shlop_29_03_2020(locus_table=fl,p_value="pval",pos="pos",snp="SNP", delta=5e5,chr="chr",thr=5e-8,trait=NULL)
+dim(n_f)
+#[1] 28 10
+
+####
+fl=fread("../generic-metal/METAANALYSIS1.TBL",data.table=F)
+
+ind=match(fl$MarkerName,ref$SNP)
+table(fl$MarkerName==ref$SNP[ind])
+
+fl=cbind(fl,pos=ref$pos[ind],chr=ref$chr[ind])
+
+maf=pmin(fl$Freq1,1-fl$Freq1)
+fl=fl[maf>=0.001,]
+
+fl=fl[!is.na(fl$chr),]
+dim(fl)
+#11760184       18
+n_f=function_for_shlop_29_03_2020(locus_table=fl,p_value="P-value",pos="pos",snp="MarkerName", delta=5e5,chr="chr",thr=5e-8,trait=NULL)
+dim(n_f)
+#[1] 30 18
+
+n_f_ma=cbind(n_f,trait="MA")
+
+
+
+n1=n_f_full_rsid[,c("SNP","pval","chr","pos","trait")]
+n2=n_f_ma[,c("MarkerName","P-value","chr","pos","trait")]
+colnames(n2)=colnames(n1)
+N=rbind(n1,n2)
+n_f=function_for_shlop_29_03_2020(locus_table=N,p_value="pval",pos="pos",snp="SNP", delta=5e5,chr="chr",thr=5e-8,trait="trait")
+dim(n_f)
+
